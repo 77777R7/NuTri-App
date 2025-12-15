9d4ad69ecbde3e12f0f9ed015939869f6fb3fed3
77777R7 <lunhoward306@gmail.com>
2025-12-14 17:00:04 -0800

    Remove unused legacy files

--- Base44MainPage/entities/navigation.ts
+++
@@ -1,3 +0,0 @@
-export type FloatingAction = 'scan' | 'assistant' | 'search';
-
-export type HomeTabKey = 'home' | 'progress' | 'favourite' | 'profile';
--- Base44MainPage/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as HomePage } from './pages/Home';
-
-export { BottomActionBar } from './components/home/BottomActionBar';
-export { StatsCards } from './components/home/StatsCards';
-export { RecentSupplements } from './components/home/RecentSupplements';
-
-export { MOCK_SUPPLEMENTS } from './entities/Supplement';
-export type { SupplementCard } from './entities/Supplement';
-export type { FloatingAction, HomeTabKey } from './entities/navigation';
